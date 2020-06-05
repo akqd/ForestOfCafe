c36342614d5c0c3c126ee0b45bc6028713382931
snu04038 <66099399+sun04038@users.noreply.github.com>
2020-06-05 21:39:46 +0900

    Add files via upload
    커뮤니터 화면 Xml으로 구현만한거 업로드함

---
+++ comunity - backup/app/src/main/java/com/example/comunity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.comunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.google.android.material.chip.Chip;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+    }
+}
