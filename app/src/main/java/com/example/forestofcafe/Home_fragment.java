00d866b8afc7c22b576f5609e05d0d38f89931ed
kangingu1227 <66099432+kangingu1227@users.noreply.github.com>
2020-06-11 00:50:38 +0900

    근처 카페 리스트 수정본

--- app/src/main/java/com/example/forestofcafe/Home_fragment.java
+++ app/src/main/java/com/example/forestofcafe/Home_fragment.java
@@ -1,20 +1,22 @@
 package com.example.forestofcafe;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeoutException;
 
 
@@ -24,24 +26,39 @@ public class Home_fragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
-    RecyclerView recyclerView;
-    RecyclerView.LayoutManager layoutManager;
-    RecyclerView.Adapter adapter;
     View v;
+
+    RecyclerView rv_MainCafeList;
+    RecyclerView.LayoutManager layoutManager;
+    MainCafeList_Adapter mAdapter;
+    private ArrayList<MainCafeList_Item> mData = new ArrayList<MainCafeList_Item>();
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         context = getActivity();
-        recyclerView = getActivity().findViewById(R.id.recyclerView);
+        rv_MainCafeList = getActivity().findViewById(R.id.rv_MainCafeList);
         layoutManager = new LinearLayoutManager(context);
-        recyclerView.setLayoutManager(layoutManager);
-        String[] Cafe_name = {"카페이름1", "카페이름2","카페이름3","카페이름1", "카페이름2", "카페이름3"};
-        String[] Cafe_distance = {"500m", "1km", "1.5km","500m", "1km", "1.5km"};
-        String[] Cafe_openclose = {"CLOSE", "OPEN", "OPEN","CLOSE", "OPEN", "OPEN"};
-        Integer[] Cafe_images = {R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_foreground};
-        adapter = new com.example.forestofcafe.MainAdapter(Cafe_name, Cafe_distance, Cafe_openclose, Cafe_images);
-        recyclerView.setAdapter(adapter);
+        rv_MainCafeList.setLayoutManager(layoutManager);
+        mAdapter = new MainCafeList_Adapter(mData);
+        rv_MainCafeList.setAdapter(mAdapter);
+
+        addCafeListItem("카페이름1", "050m", "OPEN", getResources().getDrawable(R.drawable.cafe1, null));
+        addCafeListItem("카페이름2", "100m", "CLOSE", getResources().getDrawable(R.drawable.cafe2, null));
+        addCafeListItem("카페이름3", "200m", "OPEN", getResources().getDrawable(R.drawable.cafe3, null));
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public void addCafeListItem(String cafe_name, String cafe_distance, String cafe_openclose, Drawable cafe_image) {
+        MainCafeList_Item item = new MainCafeList_Item();
+
+        item.setCafe_Name(cafe_name);
+        item.setCafe_distance(cafe_distance);
+        item.setCafe_openclose(cafe_openclose);
+        item.setCafe_Image(cafe_image);
+
+        mData.add(item);
     }
 
     @Override
@@ -49,7 +66,7 @@ public class Home_fragment extends Fragment {
                              Bundle savedInstanceState) {
         try {
             v = inflater.inflate(R.layout.fragment_menu_home, container, false);
-        }catch(InflateException e){ // 구글 Map fragment inflate 에러 무시
+        } catch (InflateException e) { // 구글 Map fragment inflate 에러 무시
         }
         return v;
     }
@@ -57,9 +74,9 @@ public class Home_fragment extends Fragment {
     @Override
     public void onDestroyView() { // 화면에서 라라질때 프래그먼트의 뷰 삭제
         super.onDestroyView();
-        if(v != null){
+        if (v != null) {
             ViewGroup parent = (ViewGroup) v.getParent();
-            if(parent!= null){
+            if (parent != null) {
                 parent.removeView(v);
             }
         }
