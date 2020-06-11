8215858c9cc9fa9a15added63f5ef136a81e725b
Jung Su Young <akqd1025@gmail.com>
2020-06-11 21:13:47 +0900

    search view - > autocompletetextview & imageview로 수정 동그랗게 이미지 추가

--- app/src/main/java/com/example/forestofcafe/Search_Activity.java
+++ app/src/main/java/com/example/forestofcafe/Search_Activity.java
@@ -3,11 +3,11 @@ package com.example.forestofcafe;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,8 +17,8 @@ import java.util.ArrayList;
 
 public class Search_Activity extends AppCompatActivity {
 
-    SearchView search_view ;
     Chip chip1 ;
+    AutoCompleteTextView act_search;
     RecyclerView rv_searchResult;
     SearchResult_Adapter sAdapter ;
     private ArrayList<SearchResult_Item> sData = new ArrayList<SearchResult_Item>();
@@ -27,8 +27,8 @@ public class Search_Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        act_search = findViewById(R.id.act_search);
         rv_searchResult = findViewById(R.id.rv_searchResult);
-        search_view = findViewById(R.id.search_view);
         chip1 = findViewById(R.id.chip1);
         sAdapter = new SearchResult_Adapter(sData);
         sAdapter.setOnItemClickListener(new SearchResult_Adapter.OnItemClickListener() {
@@ -39,25 +39,15 @@ public class Search_Activity extends AppCompatActivity {
         });
         rv_searchResult.setAdapter(sAdapter);
         rv_searchResult.setLayoutManager(new LinearLayoutManager(this));
-
-        search_view.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) { // 작성한 다음 검색 버튼을 눌렀을때 검색
-                return true; // 사용하는 곳의 반환값은 true
-            }
-            @Override
-            public boolean onQueryTextChange(String newText) { // 작성하는 순간마다 검색 됨
-                return false;
-
-            }
-        });
         addItem(getDrawable(R.drawable.cafe_image),"OPEN","카페1","카페1주소","#대충 #만듬","350km");
         addItem(getDrawable(R.drawable.cafe1),"CLOSE","카페2","카페2주소","#대충 #만듬","450km");
         addItem(getDrawable(R.drawable.cafe2),"OPEN","카페3","카페3주소","#대충 #만듬","550km");
         addItem(getDrawable(R.drawable.cafe3),"CLOSE","카페4","카페4주소","#대충 #만듬","650km");
         addItem(getDrawable(R.drawable.cafe4),"OPEN","카페5","카페5주소","#대충 #만듬","750km");
         sAdapter.notifyDataSetChanged();
+
     }
+
     public void addItem(Drawable cafe_image, String op_cl, String title, String location, String keyword, String distance){
         SearchResult_Item item = new SearchResult_Item();
         item.setCafe_image(cafe_image);
