3d6cdf2ad5e6a9b456f127cbf4c451139ded58a4
KJH <snu04038@naver.com>
2020-06-11 13:13:07 +0900

    강인구 + 김준희 합침

--- app/src/main/java/com/example/forestofcafe/Home_fragment.java
+++ app/src/main/java/com/example/forestofcafe/Home_fragment.java
@@ -17,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 
@@ -28,7 +29,7 @@ public class Home_fragment extends Fragment {
     private String mParam2;
     View v;
 
-    RecyclerView rv_MainCafeList;
+    RecyclerView rv_MainCafeList, comm_recyclerView;
     RecyclerView.LayoutManager layoutManager;
     MainCafeList_Adapter mAdapter;
     private ArrayList<MainCafeList_Item> mData = new ArrayList<MainCafeList_Item>();
@@ -48,6 +49,22 @@ public class Home_fragment extends Fragment {
         addCafeListItem("카페이름2", "100m", "CLOSE", getResources().getDrawable(R.drawable.cafe2, null));
         addCafeListItem("카페이름3", "200m", "OPEN", getResources().getDrawable(R.drawable.cafe3, null));
         mAdapter.notifyDataSetChanged();
+
+        //메인커뮤니티화면에 대한 코드 추가(김준희)
+        comm_recyclerView = getActivity().findViewById(R.id.comm_recyclerView);
+        LinearLayoutManager comm_layoutManager = new LinearLayoutManager(context);
+
+        comm_layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        comm_recyclerView.setLayoutManager(comm_layoutManager);
+
+        List<MainCommunity_Item> community_itemList = new ArrayList<>();
+        community_itemList.add(new MainCommunity_Item(R.drawable.ic_profile, R.drawable.cafe1,"커피돌이", "Be, Bridge", "넓어서 좋고 분위기가 시원시원해요~", "4.7", "10", "5"));
+        community_itemList.add(new MainCommunity_Item(R.drawable.ic_profile, R.drawable.cafe2, "커피순이", "Cafe청담", "정동극장 입장전에 한잔하기 좋아요~ 따뜻한 분위기라 힐링되네요~", "5.0", "18", "9"));
+        community_itemList.add(new MainCommunity_Item(R.drawable.ic_profile, R.drawable.cafe3, "조범석", "PS카페", "참치를 안파네", "1", "99", "99"));
+
+        MainCommunity_Adapter comm_adapter = new MainCommunity_Adapter(community_itemList);
+        comm_adapter.notifyDataSetChanged();
+        comm_recyclerView.setAdapter(comm_adapter);
     }
 
     public void addCafeListItem(String cafe_name, String cafe_distance, String cafe_openclose, Drawable cafe_image) {
---
+++ app/src/main/java/com/example/forestofcafe/MainCommunity_Adapter.java
@@ -0,0 +1,82 @@
+package com.example.forestofcafe;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainCommunity_Adapter extends RecyclerView.Adapter<MainCommunity_Adapter.ViewHolder> {
+
+    private List<MainCommunity_Item> community_itemList;
+
+    public MainCommunity_Adapter(List<MainCommunity_Item> community_itemList) {
+        this.community_itemList = community_itemList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.view_holder_main_community, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        int profile = community_itemList.get(position).getProfile();
+        int cafeimage = community_itemList.get(position).getCafe_image();
+        String id = community_itemList.get(position).getWriter_name();
+        String cafe_name = community_itemList.get(position).cafe_name;
+        String like_num = community_itemList.get(position).getLike_num();
+        String commnet_num = community_itemList.get(position).getComment_num();
+        String score = community_itemList.get(position).getScore_num();
+        String review = community_itemList.get(position).getCafe_review();
+        viewHolder.setData(profile, cafeimage, id,cafe_name, like_num, commnet_num, score, review);
+    }
+
+    @Override
+    public int getItemCount() {
+        return community_itemList.size();
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView profile;
+        private ImageView cafe_img;
+        private TextView id;
+        private TextView cafe_name;
+        private TextView like_num;
+        private TextView comment_num;
+        private TextView score;
+        private TextView review;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            profile = itemView.findViewById(R.id.comm_writerProfile);
+            cafe_img = itemView.findViewById(R.id.comm_imageview);
+            id = itemView.findViewById(R.id.comm_writerID);
+            cafe_name = itemView.findViewById(R.id.comm_cafename);
+            like_num = itemView.findViewById(R.id.comm_likenum);
+            comment_num = itemView.findViewById(R.id.comm_commentnum);
+            score = itemView.findViewById(R.id.comm_score);
+            review = itemView.findViewById(R.id.comm_review);
+        }
+
+        private void setData(int userprofile, int cafeimg, String userid, String cname, String likenum, String commnetnum, String scorenum, String cafereview){
+            profile.setImageResource(userprofile);
+            cafe_img.setImageResource(cafeimg);
+            id.setText(userid);
+            cafe_name.setText(cname);
+            like_num.setText(likenum);
+            comment_num.setText(commnetnum);
+            score.setText(scorenum);
+            review.setText(cafereview);
+        }
+    }
+}
