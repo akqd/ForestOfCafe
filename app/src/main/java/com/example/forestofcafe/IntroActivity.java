5d59b998d2b7cd6f6a7c4bac12c0dd191638845d
kangingu1227 <66099432+kangingu1227@users.noreply.github.com>
2020-06-20 14:51:25 +0900

    IntroActivity 만들어서 2초 후에 StartActivity 시작하는 형식으로 바꿈

---
+++ app/src/main/java/com/example/forestofcafe/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.example.forestofcafe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
